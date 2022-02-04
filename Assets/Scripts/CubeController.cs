118a8925d70ecd2de67497f1cbd2cd7817c60837
NadiaVorontsova <Vorontsovvaaa@gmail.com>
2022-02-04 19:12:23 +0200

    Update project

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    public GameObject Cube;
     public Vector3 Size;
     public Vector3 Rotation;
 
     private void Update()
     {
-        if (this.gameObject.transform.localScale.x < Size.x) 
+        if (Cube.transform.localScale.x < Size.x) 
         {
-            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x+0.01f, 
-                this.gameObject.transform.localScale.y+0.01f);
+            Cube.transform.localScale = new Vector3(Cube.transform.localScale.x+0.01f,
+                Cube.transform.localScale.y+0.01f);
         }
-        if (this.gameObject.transform.rotation.z < Rotation.z)
+        if (Cube.transform.rotation.z < Rotation.z)
         {
-            this.gameObject.transform.Rotate(0,0,this.gameObject.transform.localScale.z + 0.07f);
+            Cube.transform.Rotate(0,0, Cube.transform.localScale.z + 0.07f);
         }
     }
 
